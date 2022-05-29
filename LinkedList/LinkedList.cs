59fdbd9c72657afdeac7ed2e8ff9d4cf64e2cd5b
Kundan <kundan kamble>
2022-05-29 14:41:55 -0700

    [KUNDAN] Added Insert node in linked list program

---
+++ LinkedList/LinkedList.cs
@@ -0,0 +1,94 @@
+﻿using System;
+
+namespace LinkedList
+{
+    class LinkedLists
+    {
+        public Node head;
+        public Node tail;
+        public LinkedLists()
+        {
+            head = null;
+            tail = null;
+        }
+        public void AddNode(int data)
+        {
+            Node node = new Node(data);
+            if (head == null && tail == null)
+            {
+                head = node;
+                tail = node;
+            }
+            else
+            {
+                node.next = head;
+                head = node;
+            }
+        }
+
+        internal void Display()
+        {
+            Node temp = head;
+            if (temp == null)
+            {
+                Console.WriteLine("LinkedList is Empty");
+            }
+            while (temp != null)
+            {
+                Console.Write(temp.data);
+                if (temp.next != null)
+                {
+                    Console.Write(" -> ");
+                }
+                temp = temp.next;
+            }
+        }
+        public void AppendNode(int data)
+        {
+            Node node = new Node(data);
+            if (head == null)
+            {
+                head = tail = node;
+            }
+            else
+            {
+                tail.next = node;
+                tail = node;
+            }
+        }
+        public Node Search(int data)
+        {
+            Node temp = head;
+            while (temp != null)
+            {
+                if (temp.data == data)
+                {
+                    return temp;
+                }
+                temp = temp.next;
+            }
+            return null;
+        }
+        public void Insert(int data, int dataBefore)
+        {
+            Node node = new Node(data);
+            if (head == null)
+                head = tail = node;
+            else
+            {
+                Node nodeBefore = Search(dataBefore);
+                if (nodeBefore != null)
+                {
+                    node.next = nodeBefore.next;
+                    nodeBefore.next = node;
+                    if (nodeBefore == tail)
+                    {
+                        tail = node;
+                    }
+                }
+
+            }
+        }
+    }
+}
+
--- LinkedList/Program.cs
+++ LinkedList/Program.cs
@@ -1,25 +1,17 @@
-﻿using System;
-namespace LinkedList
+﻿namespace LinkedList
 {
-    class Program
+    internal class Program
     {
-        public static void Main(string[] args)
+        static void Main(string[] args)
         {
-            LinkedList linkedList = new LinkedList();
-            Node node1 = new Node(70);
-            Node node2 = new Node(30);
-            Node node3 = new Node(56);
+            LinkedLists linkedList = new LinkedLists();
 
-            linkedList.AddNode(node1);
-            linkedList.AddNode(node2);
-            linkedList.AddNode(node3);
-
-            linkedList.Display();
-
-            Node node4 = new Node(80);
-            linkedList.AppendNode(node4);
+            linkedList.AddNode(56);
+            linkedList.AppendNode(70);
+            linkedList.Insert(30, 56);
             linkedList.Display();
 
         }
     }
 }
+

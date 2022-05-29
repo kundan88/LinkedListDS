e70dc2211c521996041ea3c71ced92f2fbe064a6
Kundan <kundan kamble>
2022-05-29 13:46:57 -0700

    [KUNDAN] Added Create linked list program

---
+++ LinkedList/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace LinkedList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedLists linkedList = new LinkedLists();
+            linkedList.AddNode(70);
+            linkedList.AddNode(30);
+            linkedList.AddNode(56);
+            linkedList.Display();
+
+
+
+        }
+    }
+}
